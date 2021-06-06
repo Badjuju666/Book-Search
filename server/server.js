fc410fb749cd3e2777df367f153c27c5d3c90f1b
Julian Graves <Juliangraves96@gmail.com>
2021-06-06 03:53:42 -0700

    hmm

--- server/server.js
+++ server/server.js
@@ -16,10 +16,8 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
